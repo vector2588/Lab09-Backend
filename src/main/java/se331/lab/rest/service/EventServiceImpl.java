a2178683104f44879aa7762307393e18802f925f
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-08 23:03:49 +0700

    initial commit

---
+++ src/main/java/se331/lab/rest/service/EventServiceImpl.java
@@ -0,0 +1,32 @@
+package se331.lab.rest.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import se331.lab.rest.dao.EventDao;
+import se331.lab.rest.entity.Event;
+
+@Service
+public class EventServiceImpl implements EventService{
+    @Autowired
+    EventDao eventDao;
+    @Override
+    public Integer getEventSize() {
+        return eventDao.getEventSize();
+    }
+
+    @Override
+    public Page<Event> getEvents(Integer pageSize, Integer page) {
+        return eventDao.getEvents(pageSize, page);
+    }
+
+    @Override
+    public Event getEvent(Long id) {
+        return eventDao.getEvent(id);
+    }
+
+    @Override
+    public Event save(Event event) {
+        return eventDao.save(event);
+    }
+}
