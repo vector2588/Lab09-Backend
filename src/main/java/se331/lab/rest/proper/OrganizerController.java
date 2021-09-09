d6f501e0e7b00ffa71a86744b7071c9f05891b15
Vector2588 <natkamol082@gmail.com>
2021-09-09 16:19:01 +0700

    6.2

--- src/main/java/se331/lab/rest/proper/OrganizerController.java
+++ src/main/java/se331/lab/rest/proper/OrganizerController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se331.lab.rest.proper.OrganizerService;
+import se331.lab.rest.util.LabMapper;
 
 @RestController
 public class OrganizerController {
@@ -12,6 +13,6 @@ public class OrganizerController {
     OrganizerService organizerService;
     @GetMapping("/organizers")
     ResponseEntity<?> getOrganizers() {
-        return ResponseEntity.ok(organizerService.getAllOrganizer());
+        return ResponseEntity.ok(LabMapper.INSTANCE.getOrganizerDTO(organizerService.getAllOrganizer()));
     }
 }
